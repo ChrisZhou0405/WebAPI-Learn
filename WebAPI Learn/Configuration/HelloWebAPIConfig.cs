df32697b325cfedd9b409289ae560f363f88f85f
Daisy <996190648@qq.com>
2017-10-21 11:48:02 +0800

    start

---
+++ WebAPI Learn/Configuration/HelloWebAPIConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebAPI_Learn.Configuration
+{
+    public  static class HelloWebAPIConfig
+    {
+        public  static void Register(HttpConfiguration config)
+        {
+            //web api router
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name:"DefaultApi",
+                routeTemplate:"api/{controller}/{id}",
+                defaults:new { id = RouteParameter.Optional }
+
+                );
+        }
+    }
+}
---
+++ WebAPI Learn/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_Learn.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello World";
+        }
+        
+        public string GetName(int id)
+        {
+            return "WebApi youga"+id.ToString();
+        }
+        public string GetName(int  id,string name)
+        {
+            return "WebApi youga" + id.ToString()+name;
+        }
+    }
+}
